1da0316e7d2c59925f95914b76bc82bc96d6d40c
AgustinMarceloRemeta <agustinmarceloremeta@gmail.com>
2022-12-13 03:22:54 -0300

    Rewards

---
+++ Assets/Scrips/Reward.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ScriptableReward", menuName = "Scriptables/Reward")]
+public class Reward : MonoBehaviour
+{
+    public string nameReward;
+    public float analizysTime;
+}
---
+++ Assets/Scrips/RewardAnalyzer.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RewardAnalyzer : MonoBehaviour
+{
+    public List<Reward> _rewards;
+    [SerializeField] TextMeshProUGUI analyzerText;
+    [SerializeField] float analysisTime;
+    void Start()
+    {
+        foreach(Reward item in FindObjectsOfType<Reward>()) _rewards.Add(item); 
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Reward reward= other.GetComponent<Reward>();
+        if (reward != null) StartCoroutine(Analysis(reward));
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.GetComponent<Reward>() != null) StopAllCoroutines();
+    }
+
+    IEnumerator Analysis(Reward reward)
+    {
+        analyzerText.text = "Analizando...";
+        yield return new WaitForSeconds(reward.analizysTime);
+        _rewards.Remove(reward);
+        analyzerText.text = "Analizado";
+        yield return new WaitForSeconds(analysisTime);
+        analyzerText.text = "";
+    }
+}
